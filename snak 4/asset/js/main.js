12d649478d3eb955e3c751dd2b755f36f5c4e60d
Ga <gabri.magni.97@gmail.com>
2022-03-16 15:03:56 +0100

    snak 4

--- snak 4/asset/js/main.js
+++ snak 4/asset/js/main.js
@@ -1,1 +1,22 @@
+let nome = prompt("inserisci il tuo nome");
+let list = ["Aldo", "Giovanni", "Giacomo"];
+let sentinella = false;
 let container = document.getElementById("container");
+
+/*for (i = 0; i < list.length; i++) {
+    if (nome == list[i]) {
+        sentinella = true;
+    }
+}
+
+if (sentinella == true) {
+    container.innerHTML = `Benvenuto ${nome}`;
+} else {
+    container.innerHTML = `Il tuo nome non è in lista`;
+}*/
+
+if (list.includes(nome)) {
+    container.innerHTML = `Benvenuto ${nome}`;
+} else {
+    container.innerHTML = `Il tuo nome non è in lista`;
+}
