c17d9a1445a8155737e08175cf1999e563688b5a
Ga <gabri.magni.97@gmail.com>
2022-03-16 12:49:34 +0100

    snak 3 push

--- snak 3/asset/js/main.js
+++ snak 3/asset/js/main.js
@@ -1,1 +1,14 @@
 let container = document.getElementById("container");
+let ask = [];
+let sum = 0;
+
+for (i = 0; i < 10; i++) {
+    ask.push(parseInt(prompt("inserisci un numero")));
+    console.log(ask);
+}
+
+for (let i = 0; i < ask.length; i++) {
+    sum += ask[i];
+}
+container.innerHTML = `la somma dei numeri è: ${sum}`
+
