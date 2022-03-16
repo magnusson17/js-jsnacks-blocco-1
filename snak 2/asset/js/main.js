753fc58f4765727c4ddebf9c54410bbe92a80ad2
Ga <gabri.magni.97@gmail.com>
2022-03-16 12:12:57 +0100

    snak 2 push

--- snak 2/asset/js/main.js
+++ snak 2/asset/js/main.js
@@ -1,1 +1,11 @@
+let ask1 = prompt("inserisci una parola");
+let ask2 = prompt("inserisci una seconda parola");
 let container = document.getElementById("container");
+
+if (ask1.length > ask2.length) {
+    container.innerHTML = `${ask2}, ${ask1}`;
+} else if (ask1.length < ask2.length) {
+    container.innerHTML = `${ask1}, ${ask2}`;
+} else {
+    container.innerHTML = `le parole ${ask1} e ${ask2} hanno la stessa lunghezza`;
+}
