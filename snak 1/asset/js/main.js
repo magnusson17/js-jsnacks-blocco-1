c7d64b9e82972527ab9e493c6964908551635d8a
Ga <gabri.magni.97@gmail.com>
2022-03-16 11:46:56 +0100

    push2

--- snak 1/asset/js/main.js
+++ snak 1/asset/js/main.js
@@ -3,12 +3,12 @@ let ask2 = parseInt(prompt("inserisci un altro numero"));
 let container = document.getElementById("container");
 
 if (isNaN(ask1) || isNaN(ask2)) {
-    alert("ERRORE, inserire correttamente i dati")
+    alert("ERRORE, ricaricare la pagina e inserire correttamente i dati")
 } else {
     if (ask1 > ask2) {
-        container.innerHTML = `${ask1}`;
+        container.innerHTML = `il numero più alto è: ${ask1}`;
     } else if (ask1 < ask2) {
-        container.innerHTML = `${ask2}`;
+        container.innerHTML = `il numero più alto è: ${ask2}`;
     } else {
         container.innerHTML = `i numeri inseriti sono uguali tra loro`;
     }
